da69052cd1e81546eb46a203653d7336bf46fb38
Amy <amy.jennifer.brown>
2019-08-11 18:51:01 -0400

    strangeness

--- src/com/craftinginterpreters/tool/GenerateAST.java
+++ src/com/craftinginterpreters/tool/GenerateAST.java
@@ -26,6 +26,7 @@ public class GenerateAST {
         defineAst(outputDir, "Stmt", Arrays.asList(
                 "Block : List<Stmt> statements",
                 "Expression : Expr expression",
+                "If : Expr condition, Stmt thenBranch, Stmt elseBranch",
                 "Print : Expr expression",
                 "Var : Token name, Expr initializer"
         ));
