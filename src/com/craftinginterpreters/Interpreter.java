181582cce0f1df67f63f1ff312635ed15fdd0047
Amy <amy.jennifer.brown>
2019-08-11 19:12:04 -0400

    Added if-branches and logical And/Or

--- src/com/craftinginterpreters/Interpreter.java
+++ src/com/craftinginterpreters/Interpreter.java
@@ -74,6 +74,15 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         return null;
     }
 
+    @Override
+    public Void visitIfStmt(Stmt.If stmt) {
+        if (isTruthy(stmt.condition)){
+            execute(stmt.thenBranch);
+        } else if (stmt.elseBranch != null) {
+            execute(stmt.elseBranch);
+        }
+        return null;
+    }
 
     @Override
     public Void visitPrintStmt(Stmt.Print stmt) {
@@ -134,6 +143,19 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     @Override
     public Object visitLiteralExpr(Expr.Literal expr) {
         return expr.value;
+    }
+
+    @Override
+    public Object visitLogicalExpr(Expr.Logical expr) {
+        Object left = evaluate(expr.left);
+
+        if (expr.operator.type == TokenType.OR) {
+            if (isTruthy(left)) return left;
+        } else {
+            if (!isTruthy(left)) return left;
+        }
+
+        return evaluate(expr.right);
     }
 
     @Override
