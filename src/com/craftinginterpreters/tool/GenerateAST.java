10549789e13e955a94611eee48489a99c5d7f781
Amy <amy.jennifer.brown>
2019-08-11 18:15:02 -0400

    Added block statements and local scope

--- src/com/craftinginterpreters/tool/GenerateAST.java
+++ src/com/craftinginterpreters/tool/GenerateAST.java
@@ -24,6 +24,7 @@ public class GenerateAST {
                 "Variable : Token name"
         ));
         defineAst(outputDir, "Stmt", Arrays.asList(
+                "Block : List<Stmt> statements",
                 "Expression : Expr expression",
                 "Print : Expr expression",
                 "Var : Token name, Expr initializer"
