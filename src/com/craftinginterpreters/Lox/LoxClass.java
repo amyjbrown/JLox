10dd45fb18cb7e087359d9061550c044232f1af5
Amy <amy.jennifer.brown>
2019-08-19 16:32:10 -0400

    Added instance instantiation

--- src/com/craftinginterpreters/Lox/LoxClass.java
+++ src/com/craftinginterpreters/Lox/LoxClass.java
@@ -3,11 +3,23 @@ package com.craftinginterpreters.Lox;
 import java.util.List;
 import java.util.Map;
 
-public class LoxClass {
+public class LoxClass implements LoxCallable{
     final String name;
 
     LoxClass(String name) {
         this.name = name;
+    }
+
+    @Override
+    public Object call(Interpreter interpreter, List<Object> arguments) {
+        LoxInstance instance = new LoxInstance(this);
+        return instance;
+    }
+
+    @Override
+    public int arity() {
+        // todo add actual arity
+        return 0;
     }
 
     @Override
