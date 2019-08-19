4e0efeddd48c88fd0967f505f3e6fb01c8832cfd
Amy <amy.jennifer.brown>
2019-08-19 17:12:04 -0400

    Added setting fields on instances

--- src/com/craftinginterpreters/Lox/LoxInstance.java
+++ src/com/craftinginterpreters/Lox/LoxInstance.java
@@ -19,6 +19,10 @@ class LoxInstance {
         + "'.");
     }
 
+    void set(Token name, Object value) {
+        fields.put(name.lexeme, value);
+    }
+
     @Override
     public String toString() {
         return klass.name + " instance";
